5f1423de86f7e0c7fa430961418f834dccc2c6b5
PhilipBessong <76492088+PhilipBessong@users.noreply.github.com>
2023-12-27 10:05:05 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'wodbox_a',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
